67a7bc5a91111d4fe57c853683e3f09b7874ab76
Héctor Ramos <hector@parse.com>
2012-08-28 18:09:41 -0700

    Initial commit

---
+++ Geolocations/SearchViewController.h
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.h
+//  Geolocations
+//
+//  Created by Héctor Ramos on 8/16/12.
+//  Copyright (c) 2012 Parse, Inc. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface SearchViewController : UIViewController <MKMapViewDelegate>
+
+@property (nonatomic, strong) IBOutlet MKMapView *mapView;
+@property (nonatomic, strong) IBOutlet UISlider *slider;
+
+- (void)setInitialLocation:(CLLocation *)aLocation;
+
+@end
